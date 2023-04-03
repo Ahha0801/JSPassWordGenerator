9147b4bd43211a822acf851b977fd5f231b716b7
Ahha0801 <128229173+Ahha0801@users.noreply.github.com>
2023-04-02 22:54:59 -0500

    updates

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var choiceArr= [];
+var uppercaseArr = ["A","B","C","D","E","F","G","H","I", "J", "K","L", "M", "N","O", "P","Q", "R", "S","T","U", "V","W","X","Y","Z"];
+var lowercaseArr= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+var specicalcharactersArr= [ "!", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", ];
+var numberArr=["1","2","3","4","5","6","7","8","9","0"];
+var CharacterLenght= [8-20]
+
+function passwordcriteria (){
+    CharactersLength = prompt ("How long do you want the password to be?InPlease select a value between 8 and 128")
+      if(isNaN(CharacterLenght) || CharacterLength < 8 || CharacterLenght >128) {
+        alert( "Character Lenght has be longer, please try again");
+        return false;
+       
+      }
+var includeSpecialCharacters = confirm("Do you want to include special characters?")
+
+var includeUppercase = confirm ("Do you want to include uppercase letters?")
+var includeLowercase = confirm( "Do you want to include lowercase letters?"
+var includeNumbers = confirm("Do you want to include numbers?")
+}
+
+
+function generatePassword() {thing in here
+}
+
 
 // Write password to the #password input
 function writePassword() {
